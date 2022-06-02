d8c49e5149dac5ac8b19437d15ef8246aa059069
Jérémie Arcidiacono <49244181+jeremie-arcidiacono@users.noreply.github.com>
2022-06-02 16:15:27 +0200

    Add jQuery on every page

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -20,6 +20,9 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">        
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
 
+        <!-- Import jQuery -->
+        <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
+
     </head>
     <body class="font-sans antialiased">
         @include('layouts.navigation')
