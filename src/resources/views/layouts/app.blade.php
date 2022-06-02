cc7e6e47093fad87eaef5865079a3779d0b180e0
Jérémie Arcidiacono <49244181+jeremie-arcidiacono@users.noreply.github.com>
2022-06-02 10:24:06 +0200

    Create main layout of the app

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -15,22 +15,40 @@
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- Import Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">        
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+        @include('layouts.navigation')
+        <div class="min-h-screen bg-gray-100 float-end d-flex flex-column" style="width: 83%">
 
             <!-- Page Heading -->
             <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <div class="w-50 py-6 px-4 sm:px-6 lg:px-8 float-start">
                     {{ $header }}
                 </div>
+                @if (isset($addButton))
+                <div class="w-25 pt-6 px-4 sm:px-6 lg:px-8 float-end">
+                    <button type="button" class="btn btn-success">
+                        <i class="bi bi-plus"></i>
+                        {{ $addButton }}
+                    </button>
+                </div>
+                @endif
             </header>
 
             <!-- Page Content -->
-            <main>
+            <main class="float-end">
                 {{ $slot }}
             </main>
+
+            @include('layouts.footer')
         </div>
+
+        <!-- Import Bootstrap -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     </body>
 </html>
---
+++ src/resources/views/layouts/footer.blade.php
@@ -0,0 +1,17 @@
+
+  <footer class="footer mt-auto d-flex flex-wrap justify-content-between align-items-center p-3 border-top float-end">
+    <div class="col-md-4 d-flex align-items-center">
+
+      <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+        <x-application-logo class="bi pe-none me-2" width="40" height="32"/>
+      </a>
+      <span class="mb-3 mb-md-0 text-muted">Smart Printers Supplies, 2022</span>
+    </div>
+
+    <div class="col-md-4 d-flex align-items-center pe-3 justify-content-end">
+        <span class="mb-3 mb-md-0 text-muted">
+            Made by 
+            <a href="https://github.com/jeremie-arcidiacono/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">Jérémie Arcidacono</a>
+        </span>
+    </div>
+  </footer>
