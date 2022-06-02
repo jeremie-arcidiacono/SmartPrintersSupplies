29661d53354ee75d304310dd675ad6cb588acc57
Jérémie Arcidiacono <49244181+jeremie-arcidiacono@users.noreply.github.com>
2022-06-02 10:21:34 +0200

    Rename all API routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -19,31 +19,31 @@ use App\Http\Controllers\api\EventController;
 */
 
 
-Route::get('/printers', [PrinterController::class, 'index'])->name('printers.index');
-Route::get('/printers/{printer}', [PrinterController::class, 'show'])->name('printers.show');
-Route::post('/printers', [PrinterController::class, 'store'])->name('printers.store');
-Route::put('/printers/{printer}', [PrinterController::class, 'update'])->name('printers.update');
-Route::delete('/printers/{printer}', [PrinterController::class, 'destroy'])->name('printers.destroy')->withTrashed();
-Route::get('/printers/{printer}/events', [PrinterController::class, 'events'])->name('printers.events');
+Route::get('/printers', [PrinterController::class, 'index'])->name('api.printers.index');
+Route::get('/printers/{printer}', [PrinterController::class, 'show'])->name('api.printers.show');
+Route::post('/printers', [PrinterController::class, 'store'])->name('api.printers.store');
+Route::put('/printers/{printer}', [PrinterController::class, 'update'])->name('api.printers.update');
+Route::delete('/printers/{printer}', [PrinterController::class, 'destroy'])->name('api.printers.destroy')->withTrashed();
+Route::get('/printers/{printer}/events', [PrinterController::class, 'events'])->name('api.printers.events');
 
 
-Route::get('/models', [PrinterModelController::class, 'index'])->name('models.index');
-Route::get('/models/{printerModel}', [PrinterModelController::class, 'show'])->name('models.show');
-Route::post('/models', [PrinterModelController::class, 'store'])->name('models.store');
-Route::put('/models/{printerModel}', [PrinterModelController::class, 'update'])->name('models.update');
-Route::delete('/models/{printerModel}', [PrinterModelController::class, 'destroy'])->name('models.destroy')->withTrashed();
+Route::get('/models', [PrinterModelController::class, 'index'])->name('api.models.index');
+Route::get('/models/{printerModel}', [PrinterModelController::class, 'show'])->name('api.models.show');
+Route::post('/models', [PrinterModelController::class, 'store'])->name('api.models.store');
+Route::put('/models/{printerModel}', [PrinterModelController::class, 'update'])->name('api.models.update');
+Route::delete('/models/{printerModel}', [PrinterModelController::class, 'destroy'])->name('api.models.destroy')->withTrashed();
 
-Route::get('/supplies', [SupplyController::class, 'index'])->name('supplies.index');
-Route::get('/supplies/{supply}', [SupplyController::class, 'show'])->name('supplies.show');
-Route::post('/supplies', [SupplyController::class, 'store'])->name('supplies.store');
-Route::put('/supplies/{supply}', [SupplyController::class, 'update'])->name('supplies.update');
-Route::delete('/supplies/{supply}', [SupplyController::class, 'destroy'])->name('supplies.destroy')->withTrashed();
+Route::get('/supplies', [SupplyController::class, 'index'])->name('api.supplies.index');
+Route::get('/supplies/{supply}', [SupplyController::class, 'show'])->name('api.supplies.show');
+Route::post('/supplies', [SupplyController::class, 'store'])->name('api.supplies.store');
+Route::put('/supplies/{supply}', [SupplyController::class, 'update'])->name('api.supplies.update');
+Route::delete('/supplies/{supply}', [SupplyController::class, 'destroy'])->name('api.supplies.destroy')->withTrashed();
 
-Route::get('/supplies/{supply}/compatibilities', [SupplyController::class, 'indexCompatibility'])->name('supplies.indexCompatibility');
-Route::get('/models/{printerModel}/compatibilities', [PrinterModelController::class, 'indexCompatibility'])->name('models.indexCompatibility');
-Route::post('/models/{printerModel}/compatibilities/', [PrinterModelController::class, 'storeCompatibility'])->name('models.storeCompatibility');
-Route::delete('/models/{printerModel}/compatibilities/{supply}', [PrinterModelController::class, 'destroyCompatibility'])->name('models.destroyCompatibility');
+Route::get('/supplies/{supply}/compatibilities', [SupplyController::class, 'indexCompatibility'])->name('api.supplies.indexCompatibility');
+Route::get('/models/{printerModel}/compatibilities', [PrinterModelController::class, 'indexCompatibility'])->name('api.models.indexCompatibility');
+Route::post('/models/{printerModel}/compatibilities/', [PrinterModelController::class, 'storeCompatibility'])->name('api.models.storeCompatibility');
+Route::delete('/models/{printerModel}/compatibilities/{supply}', [PrinterModelController::class, 'destroyCompatibility'])->name('api.models.destroyCompatibility');
 
-Route::get('/events', [EventController::class, 'index'])->name('events.index');
-Route::get('/events/{event}', [EventController::class, 'show'])->name('events.show');
+Route::get('/events', [EventController::class, 'index'])->name('api.events.index');
+Route::get('/events/{event}', [EventController::class, 'show'])->name('api.events.show');
 
