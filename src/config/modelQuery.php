a56e457e6cf96a8fb17d72217dedcd102f66a99f
Jérémie Arcidiacono <49244181+jeremie-arcidiacono@users.noreply.github.com>
2022-06-08 08:22:12 +0200

    Minor change in modelQuery config

--- src/config/modelQuery.php
+++ src/config/modelQuery.php
@@ -3,8 +3,8 @@
 return [
 
     'printer_perPage' => 25,     // Default number of printers per page
-    'printer_sortOrder' => 'asc', // Default sortBy order
-    'printer_sortColumn' => 'created_at', // Default sortBy column
+    'printer_sortOrder' => 'desc', // Default sortBy order
+    'printer_sortColumn' => 'idPrinter', // Default sortBy column
     'printer_nbEvent' => 15,      // Default number of events to display on the detail page of a printer
 
     'printerModel_perPage' => 25,     // Default number of models per page
@@ -12,9 +12,9 @@ return [
 
     'supply_perPage' => 25,     // Default number of supplies per page
     'supply_sortOrder' => 'desc', // Default sortBy order
-    'supply_sortColumn' => 'created_at', // Default sortBy column
+    'supply_sortColumn' => 'idSupply', // Default sortBy column
 
     'event_perPage' => 50,     // Default number of event per page
     'event_sortOrder' => 'desc', // Default sortBy order
-    'event_sortColumn' => 'created_at', // Default sortBy column
+    'event_sortColumn' => 'idEvent', // Default sortBy column
 ];
