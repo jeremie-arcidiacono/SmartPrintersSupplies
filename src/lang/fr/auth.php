a54d06bd5320384331dbcc4e94bd544924ac8639
Jérémie Arcidiacono <49244181+jeremie-arcidiacono@users.noreply.github.com>
2022-06-16 09:58:12 +0200

    Add french translations for error messages

---
+++ src/lang/fr/auth.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Ces informations d\'identification sont incorrectes.',
+    'password' => 'Le mot de passe fourni est incorrect.',
+    'throttle' => 'Trop de tentatives de connexion. Veuillez réessayer dans :seconds secondes.',
+    'disabled' => 'Votre compte a été désactivé.',
+    'autoDisabled' => 'Vous ne pouvez pas activer ou désactiver votre propre compte.',
+
+];
